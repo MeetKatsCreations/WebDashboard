87d90794cb82b67737f6dda948ebfeef291e6479
Anshika2608 <anshika2210142@akgec.ac.in>
2025-03-10 13:15:19 +0530

    setup_done

---
+++ Server/index.js
@@ -0,0 +1,9 @@
+const express=require("express")
+const cors=require("cors")
+const app=express();
+require("./Config/Config")
+app.use(express.json())
+const port=process.env.PORT||3000
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`)
+})
